243d91cd1b0fb3e9a365d1f68aa872af711316c5
umidjon_developer <umidjongafforov175@gmail.com>
2025-04-14 23:48:08 +0500

    umidjon-blog

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import { ChildProps } from "@/types";
+import React from "react";
+import Navbar from "./_components/navbar";
+import Footer from "./_components/footer";
+
+function Layout({ children }: ChildProps) {
+  return (
+    <div>
+      <Navbar />
+      <div className="container">{children}</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
