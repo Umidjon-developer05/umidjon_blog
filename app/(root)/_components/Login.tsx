83836bd75c1bf53cabdbc9aaaf85fb8d1ca36720
Umidjon-mobile <umidjongafforov175@gmail.com>
2025-05-18 17:27:13 +0500

    blog-mobile-navbar

--- app/(root)/_components/Login.tsx
+++ app/(root)/_components/Login.tsx
@@ -14,6 +14,7 @@ import { Separator } from '@/components/ui/separator'
 import { signIn } from 'next-auth/react'
 import { useState } from 'react'
 
+
 function LoginPage() {
 	const [isLoading, setIsLoading] = useState<{
 		google: boolean
